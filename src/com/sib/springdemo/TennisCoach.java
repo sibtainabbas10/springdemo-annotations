a8377b9b765862e1d1b77990726ae3f353d75837
sibtainabbas10 <sibtain.abbas10@gmail.com>
2017-08-21 18:00:27 +0500

    - dependency injection using Java annotations: setter injection

--- src/com/sib/springdemo/TennisCoach.java
+++ src/com/sib/springdemo/TennisCoach.java
@@ -9,12 +9,26 @@ import org.springframework.stereotype.Component;
 // Spring Container
 @Component
 public class TennisCoach implements ICoach {
+
     private IFortuneService fortuneService;
 
+    // define a default constructor
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    @Autowired
+    public void doSomeCrazyStuff(IFortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+        this.fortuneService = fortuneService;
+    }
+
+/*
     @Autowired
     public TennisCoach(IFortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
+*/
 
     @Override
     public String getDailyWorkout() {
